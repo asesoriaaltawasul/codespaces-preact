e2adb7bda3683bd3272d75b4b7355824b61a94de
asesoriaaltawasul <128938552+asesoriaaltawasul@users.noreply.github.com>
2023-03-26 03:51:01 +0200

    Update index.js

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,9 +3,9 @@ import style from './style.module.css';
 export default function Home() {
 	return (
 		<div class={style.home}>
-			<h1>GitHub Codespaces <span class={style.heart}>♥️</span> Preact</h1>
+			<h1>GitHub Codespaces <span class={style.heart}>♥️</span> ASESORÍA AL-TAWASUL</h1>
 			<img src="/assets/Octocat.png" alt="Mona" class={style.octocat} />
-			<p>This is the Home component.</p>
+			<p> Menú.</p>
 		</div>
 	);
 }
